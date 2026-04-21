28e770ddbcc7a509e2ebfa9bbb0490680fe760b8
johasanchez21 <joha.sanchez@duocuc.cl>
2026-04-21 13:42:49 -0400

    Feign
    se añadio la dependencia de feign y se arreglaron errores al crear tablas en la bd

--- BioTienda/src/main/java/com/example/BioTienda/model/Reporte.java
+++ BioTienda/src/main/java/com/example/BioTienda/model/Reporte.java
@@ -4,12 +4,14 @@ package com.example.BioTienda.model;
 import java.util.Date;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @Table(name = "Reporte")
 @AllArgsConstructor
